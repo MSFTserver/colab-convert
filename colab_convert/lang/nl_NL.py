595dcbb016a023576ccd72bee09a20fab095442f
Aetherialdnb <101430108+Aetherialdnb@users.noreply.github.com>
2022-04-13 22:18:47 +0200

    Update nl_NL.py

--- colab_convert/lang/nl_NL.py
+++ colab_convert/lang/nl_NL.py
@@ -12,56 +12,56 @@
 #############
 
 # default messages
-is_warn = '[WARN]'
+is_warn = '[WAARSCHUW]'
 is_on = '[OK]'
-is_off = '[NOT]'
-lang_detected_msg = 'Detected as a supported Language'
-outputs_msg = 'showing outputs from cc'
-convert_magic_msg = 'convert magic commands'
-un_comment_msg = 'commenting out unsupported magic commands'
-imports_msg = 'keeping new imports made by cc'
-add_imports_cell_msg = 'adding new imports to the top of the file'
-help_called_msg = 'help message called'
-file_ext_msg = 'file must be .ipynb or .py'
-set_output_ext_msg = 'setting output file to'
-specify_file_msg = 'please specify atleast one file to convert'
-usage_msg = 'Usage: colab-convert <input_file> <output_file> <extra_flags>'
-un_command_det_msg = 'unsupported command is detected!'
-comment_un_cmd_msg = 'commenting out unsupported command'
-def_set_ret_mag_msg = 'default settings is retaining magic commands'
-ret_mag_det_msg = '--retain-magic is detected, new imports will NOT be made'
+is_off = '[NIET]'
+lang_detected_msg = 'gedetecteerd als een ondersteunde taal'
+outputs_msg = 'toont uitvoer van cc'
+convert_magic_msg = 'converteer magische commandos'
+un_comment_msg = 'commenting out niet ondersteunde magische commandos'
+imports_msg = 'behoud nieuwe imports gemaakt door cc'
+add_imports_cell_msg = 'voeg nieuwe imports toe aan de bovenkant van het bestand'
+help_called_msg = 'help bericht opgevraagd'
+file_ext_msg = 'bestand moet .ipynb or .py zijn'
+set_output_ext_msg = 'uitvoerbestand instellen op'
+specify_file_msg = 'specifieer minimaal één bestand om te converteren alstublieft'
+usage_msg = 'Gebruik: colab-convert <invoer_bestand> <uitvoer_bestand> <extra_flags>'
+un_command_det_msg = 'niet ondersteund commando gedetecteerd!'
+comment_un_cmd_msg = 'commenting out niet ondersteunde commandos'
+def_set_ret_mag_msg = 'standaard instellingen behoudt magische commandos'
+ret_mag_det_msg = '--retain-magic is gedetecteerd, nieuwe imports worden NIET gemaakt'
 convert_time_msg = 'conversion took'
-log_file_msg = 'log file created'
-ac_over_nc_msg = '--auto-comment (-ac) takes presidence over --no-comment (-nc)'
-ac_over_nc_fall_msg = 'using --auto-comment (-ac)'
-rm_over_cm_msg = '--retain-magic (-rm) takes presidence over --convert-magic (-cm)'
-rm_over_cm_fall_msg = 'using --retain-magic (-rm)'
-cmd_det_msg = 'command detected'
+log_file_msg = 'log bestand aangemaakt'
+ac_over_nc_msg = '--auto-comment (-ac) heeft voorrang op --no-comment (-nc)'
+ac_over_nc_fall_msg = 'gebruik --auto-comment (-ac)'
+rm_over_cm_msg = '--retain-magic (-rm) heeft voorrang op --convert-magic (-cm)'
+rm_over_cm_fall_msg = 'gebruik --retain-magic (-rm)'
+cmd_det_msg = 'commando detecteerd'
 
 # default words
-convert_wrd = 'convert'
+convert_wrd = 'converteer'
 converted_wrd = f'{convert_wrd}ed'
-finished_wrd = 'finished'
-to_wrd = 'to'
-seconds_wrd = 'seconds'
-input_wrd = 'input'
-output_wrd = 'output'
+finished_wrd = 'klaar'
+to_wrd = 'naar'
+seconds_wrd = 'secondes'
+input_wrd = 'invoer'
+output_wrd = 'uitvoer'
 
 # default options
 flags_desc = {
-    'Colab-Convert': '\nall flags are optional and have set defaults for best results\nuse flags to enable or disable certain functions on/off by default\n',
-    'Usage:': 'colab-convert <input_file> <output_file> <flags>',
-    'Example:': 'colab-convert in.ipynb out.py -nc -rm -o\n',
-    '<input_file>:': 'input file to convert',
-    '<output_file>:': 'output file to write to',
-    '<flags>:': 'extra flags to pass to the converter\n',
-    '--help': '(-h)\n  Show this help message\n',
-    'Default Flags Set (defaults are determined by input file)': '\n  ipynb input file:\n    [YES] convert magic , [YES] auto comment , [YES] imports , [NO] Outputs\n  py input file:\n    [NO] convert magic , [NO] auto comment , [NO] imports , [NO] Outputs\n',
-    'Available Flags': '\n  toggle certain items on or off\n',
-    '  --retain-magic': ' (-rm)  : Keep magic commands in the output\n      .py default    [ON]\n      .ipynb default [OFF]',
-    '  --convert-magic': ' (-cm) : Convert magic commands to python code\n      .py default    [OFF]\n      .ipynb default [ON]',
-    '  --auto-comment': ' (-ac)  : Convert unsupported magic commands to comments\n      .py default    [OFF]\n      .ipynb default [ON]',
-    '  --no-comment': ' (-nc)    : Keep unsupported magic commands\n      .py default    [ON]\n      .ipynb default [OFF]',
-    '  --no-imports': ' (-ni)    : Do not add imports from converted magic commands\n      .py default    [OFF]\n      .ipynb default [OFF]',
-    '  --outputs': ' (-o)        : Outputs to console of conversions and commented lines.\n      .py default    [OFF]\n      .ipynb default [OFF]',
+    'Colab-Convert': '\nalle flags zijn optioneel en hebben standaard instellingen voor het beste resultaat\ngebruik flags om bepaalde functies in of uit te schakelen\n',
+    'Gebruik:': 'colab-convert <invoer_bestand> <uitvoer_bestand> <flags>',
+    'Voorbeeld:': 'colab-convert in.ipynb out.py -nc -rm -o\n',
+    '<invoer_bestand>:': 'invoer bestand om te converteren',
+    '<uitvoer_bestand>:': 'uitvoer bestand om naar te schrijven',
+    '<flags>:': 'extra flags om door te geven aan de converter\n',
+    '--help': '(-h)\n  Laat dit hulp bericht zien\n',
+    'Standaard Flags ingesteld (standaardinstellingen zijn bepaald door invoer bestand)': '\n  ipynb invoer bestand:\n    [JA] convert magic , [JA] auto comment , [JA] invoer , [NEE] uitvoer\n  py invoer bestand:\n    [NEE] convert magic , [NEE] auto comment , [NO] invoer , [NEE] Uitvoer\n',
+    'Beschikbare Flags': '\n  Zet bepaalde items aan of uit\n',
+    '  --retain-magic': ' (-rm)  : Behoud magische commandos in de uitvoer\n      .py default    [AAN]\n      .ipynb default [UIT]',
+    '  --convert-magic': ' (-cm) : Converteer magische commandos naar python code\n      .py default    [UIT]\n      .ipynb default [AAN]',
+    '  --auto-comment': ' (-ac)  : Converteer niet ondersteunde magische commandos naar comments\n      .py default    [UIT]\n      .ipynb default [AAN]',
+    '  --no-comment': ' (-nc)    : Behoud niet ondersteunde magische commandos\n      .py default    [AAN]\n      .ipynb default [UIT]',
+    '  --no-imports': ' (-ni)    : Voeg imports van geconverteerde magicsche commandos niet toe\n      .py default    [UIT]\n      .ipynb default [AAN]',
+    '  --outputs': ' (-o)        : Uitvoer naar console van conversies en commented lines.\n      .py default    [UIT]\n      .ipynb default [AAN]',
 }
