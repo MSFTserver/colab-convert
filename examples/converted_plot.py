678907325b35384a4f535bca27cc9558a3b26147
MSFTserver <MSFTserver@gmail.com>
2022-04-19 15:26:57 -0700

    add new examples for testing

--- examples/converted_plot.py
+++ examples/converted_plot.py
@@ -1,11 +1,13 @@
 # %%
 # !! {"metadata":{
-# !!   "id":"colab-convert"
+# !!   "id":"cc-imports"
 # !! }}
 
 #<cc-imports>
+
 import subprocess
 import os
+import shutil
 
 # %%
 # !! {"metadata":{
@@ -25,32 +27,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shutil
 
+# test nesting and subprocesses
 if True:
+    sub_p_res = subprocess.run(['touch', 'test_file.txt'], stdout=subprocess.PIPE).stdout.decode('utf-8') #<cc-cm>
+    print(sub_p_res) #<cc-cm>
     sub_p_res = subprocess.run(['git', 'clone', 'https://github.com/MSFTserver/pytorch3d-lite.git'], stdout=subprocess.PIPE).stdout.decode('utf-8') #<cc-cm>
     print(sub_p_res) #<cc-cm>
-    os.chdir('pytorch3d-lite') #<cc-cm>
+    pip_sub_p_res = subprocess.run(['pip', 'list', '--outdated'], stdout=subprocess.PIPE).stdout.decode('utf-8') #<cc-cm>
+    print(pip_sub_p_res) #<cc-cm>
 
+#test directory magic
+os.listdir() #<cc-cm>
 os.chdir('..') #<cc-cm>
-shutil.rmtree('pytorch3d-lite')
+os.listdir() #<cc-cm>
+shutil.rmtree('pytorch3d-lite') #<cc-cm>
+os.makedirs('test_dir/test_subdir') #<cc-cm>
+cat_read_file = open('test_file.txt', 'r') #<cc-cm>
+cat_read_text = cat_read_file.read() #<cc-cm>
+print(cat_read_text) #<cc-cm>
+cat_read_file.close() #<cc-cm>
+shutil.move('test_file.txt', 'test_dir/test_file.txt') #<cc-cm>
+os.listdir('test_dir') #<cc-cm>
+shutil.copy('test_dir/test_file.txt', 'test_dir/test_subdir/test_file.txt') #<cc-cm>
+os.listdir() #<cc-cm>
+os.remove('test_file.txt') #<cc-cm>
+os.listdir() #<cc-cm>
+shutil.move('test_dir/test_file.txt', 'test_dir/test_subdir/test_file.txt') #<cc-cm>
 
+# test environment magic
 for k, v in os.environ.items(): #<cc-cm>
     print(f'{k}={v}') #<cc-cm>
-
 os.environ['PATH'] #<cc-cm>
-
 os.environ['TEST_COLAB_CONVERT'] = 'HELLO' #<cc-cm>
-
 os.environ['TEST_COLAB_CONVERT'] = 'HELLO' #<cc-cm>
-
+os.environ['TEST_COLAB_CONVERT'] #<cc-cm>
+os.environ['TEST_COLAB_CONVERT'] = 'GOODBYE' #<cc-cm>
+os.environ['TEST_COLAB_CONVERT'] = 'GOODBYE' #<cc-cm>
 os.environ['TEST_COLAB_CONVERT'] #<cc-cm>
 
-os.environ['TEST_COLAB_CONVERT'] = 'GOODBYE' #<cc-cm>
-
-os.environ['TEST_COLAB_CONVERT'] = 'GOODBYE' #<cc-cm>
-
-os.environ['TEST_COLAB_CONVERT'] #<cc-cm>
-
-#<cc-ac> %testing
+#<cc-ac> %notValid
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
--- examples/plot.py
+++ examples/plot.py
@@ -14,32 +14,39 @@
 # !! }}
 import matplotlib.pyplot as plt
 import numpy as np
-import shutil
 
+# test nesting and subprocesses
 if True:
+    !touch test_file.txt
     !git clone https://github.com/MSFTserver/pytorch3d-lite.git
-    %cd pytorch3d-lite
+    %pip list --outdated
 
+#test directory magic
+%ls
 %cd ..
-shutil.rmtree('pytorch3d-lite')
+%ls
+%rm -r pytorch3d-lite
+%mkdir test_dir/test_subdir
+%cat test_file.txt
+%mv test_file.txt test_dir
+%ls test_dir
+%cp test_dir/test_file.txt test_dir/test_subdir/test_file.txt
+%ls
+%rm test_file.txt
+%ls
+%mv test_dir/test_file.txt test_dir/test_subdir/test_file.txt
 
+# test environment magic
 %env
-
 %env PATH
-
 %env TEST_COLAB_CONVERT=HELLO
-
 %set_env TEST_COLAB_CONVERT=HELLO
-
 %env TEST_COLAB_CONVERT
-
 %env TEST_COLAB_CONVERT GOODBYE
-
 %set_env TEST_COLAB_CONVERT GOODBYE
-
 %env TEST_COLAB_CONVERT
 
-%testing
+%notValid
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
